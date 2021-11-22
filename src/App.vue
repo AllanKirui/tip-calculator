de1d822e3be5d13ab02acd5a6480e89336590693
AllanKirui <nallawilljr@gmail.com>
2021-11-22 16:29:07 +0300

    Add global styling

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header></header>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+:root {
+  /* Colors */
+  --color-light-sea-green: #26c0ac;
+  --color-deep-jungle-green: #00494d;
+  --color-wintergreen-dream: #5e7a7d;
+  --color-morning-blue: #7f9d9f;
+  --color-columbia-blue: #c5e4e7;
+  --color-mint-cream: #f4fafa;
+  --color-white: #ffffff;
+
+  /* Font sizes */
+  --font-size-13: 13px;
+  --font-size-16: 16px;
+  --font-size-20: 20px;
+  --font-size-24: 24px;
+  --font-size-32: 32px;
+  --font-size-48: 48px;
+
+  /* Fonts */
+  --font-space-mono: "Space Mono", sans-serif;
+}
+
+html {
+  font-size: 100%;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: inherit;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  font-family: var(--font-space-mono);
+  font-weight: 700;
+  background-color: var(--color-columbia-blue);
+}
+
+p {
+  margin: 0;
+}
+
+/* Flex box styles */
+.flex {
+  display: flex;
+}
+
+.flex-fd-c {
+  flex-direction: column;
+}
+
+.flex-jc-c {
+  justify-content: center;
+}
+
+.flex-jc-sb {
+  justify-content: space-between;
+}
+
+.flex-ai-c {
+  align-items: center;
 }
 </style>
