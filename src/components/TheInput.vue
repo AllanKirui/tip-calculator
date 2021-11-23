e68e490a7904597479099b1cdf5924236312f8db
AllanKirui <nallawilljr@gmail.com>
2021-11-23 11:48:09 +0300

    Add update to html for TheInput component

--- src/components/TheInput.vue
+++ src/components/TheInput.vue
@@ -2,7 +2,7 @@
   <form>
     <div class="form-control">
       <label for="bill">Bill</label>
-      <input id="bill" name="bill" type="text" />
+      <input name="bill" type="text" />
       <img
         src="../assets/images/icon-dollar.svg"
         class="icon"
@@ -12,7 +12,7 @@
 
     <div class="tip">
       <div class="tip-title">Select Tip %</div>
-      <div class="tip-control">
+      <div class="tip-controls">
         <button>5%</button>
         <button>10%</button>
         <button>15%</button>
@@ -27,7 +27,7 @@
 
     <div class="form-control">
       <label for="people">Number of People</label>
-      <input id="people" name="people" type="text" />
+      <input name="people" type="text" />
       <img
         src="../assets/images/icon-person.svg"
         class="icon"
