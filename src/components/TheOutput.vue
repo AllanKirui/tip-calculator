09e0423f07970155d2cbe4b572f82237a0ca51ec
AllanKirui <nallawilljr@gmail.com>
2021-11-23 21:59:32 +0300

    Create TheOutput component

---
+++ src/components/TheOutput.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="output-wrapper">
+    <div class="output-control flex flex-jc-sb">
+      <div class="label-wrapper flex flex-fd-c">
+        <p class="label">Tip Amount</p>
+        <p class="sub-label">/ person</p>
+      </div>
+      <div class="results-wrapper">
+        <p class="results">$4.27</p>
+      </div>
+    </div>
+
+    <div class="output-control flex flex-jc-sb">
+      <div class="label-wrapper flex flex-fd-c">
+        <p class="label">Total</p>
+        <p class="sub-label">/ person</p>
+      </div>
+      <div class="results-wrapper">
+        <p class="results">$32.79</p>
+      </div>
+    </div>
+
+    <div class="button-wrapper">
+      <button>Reset</button>
+    </div>
+  </div>
+</template>
