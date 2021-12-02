6a28cceef8574ec87a38481f42647df4f5f2e765
AllanKirui <nallawilljr@gmail.com>
2021-12-02 18:41:55 +0300

    Add update to global styles

--- src/App.vue
+++ src/App.vue
@@ -168,6 +168,8 @@ header {
   padding: 2rem;
   border-radius: 20px;
   background-color: var(--color-white);
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px,
+    rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
 }
 
 .attribution {
